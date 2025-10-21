9ec3a5e7e24a36892baf1cc636df6fb828f77458
Your Name <you@example.com>
2025-10-21 23:52:43 +0700

    fix

--- backend/src/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-
-router.get("/tag/:tag", productController.getByTag);
-router.get("/new", productController.getNew);
-router.get("/category/:slug", productController.getByCategory);
-
-module.exports = router;
--- backend/src/services/categoryService.js
+++
@@ -1,7 +0,0 @@
-const Category = require("../models/Category");
-
-exports.getAllCategories = async () => {
-  return await Category.find({ is_active: true })
-    .sort({ name: 1 })
-    .select("_id name slug image_url parent_id");
-};

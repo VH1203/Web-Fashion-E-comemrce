c6ccff94eefb3291c5310eb9e68b513634d4a4a1
Your Name <you@example.com>
2025-11-05 23:01:21 +0700

    fid

--- backend/src/app.js
+++ backend/src/app.js
@@ -58,9 +58,7 @@ const productVariant = require("./routes/productVariantRoutes");
 // const refundRoutes = require("./routes/refundRoutes");
 // const reviewRoutes = require("./routes/reviewRoutes");
 // const ticketRoutes = require("./routes/ticketRoutes");
- const shopRoutes = require("./routes/shopRoutes");
 // const adminRoutes = require("./routes/adminRoutes");
-const transactionRoutes = require("./routes/transactionRoutes");
 
 app.use(
   cors({
